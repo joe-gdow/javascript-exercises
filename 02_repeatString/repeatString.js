2bde0f686f370c204345eb210e07d36e65006136
joe <joe.gdowik@gmail.com>
2022-02-13 10:20:56 -0500

    First Commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, amt) {
+    var newStr = "";
+    if (amt < 0) {
+        return 'ERROR'
+    }
+    for (i=0; i<amt; i++) {
+        newStr += str;
+    }
+    return newStr
 };
 
 // Do not edit below this line
