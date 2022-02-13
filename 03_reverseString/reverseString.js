fef48b915ea337d7657530764436f7cc4c80aa15
joe <joe.gdowik@gmail.com>
2022-02-13 10:30:54 -0500

    03_reverseString complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const array = str.split('');
+    const revArr = array.reverse();
+    
+    return revArr.join('')
 };
 
 // Do not edit below this line
