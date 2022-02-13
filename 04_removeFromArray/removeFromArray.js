afe452142a4366f48a9ff282ac114343d6a7ce2c
joe <joe.gdowik@gmail.com>
2022-02-13 14:28:03 -0500

    exercise 04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...nums) {
+    for (arg of nums) {
+        if (array.includes(arg)) {
+            console.log(arg + "is in " + array);
+            array.splice(array.indexOf(arg), 1);
+            console.log(array);
+        }
+        else {
+            console.log(arg + "is not in " + array);
+            continue
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
