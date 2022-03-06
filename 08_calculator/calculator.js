cebceab1b72d2b41d9add09b9c3cf685aaa230af
joe <joe.gdowik@gmail.com>
2022-03-05 19:19:55 -0500

    took a little break.  back on it.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,56 @@
-const add = function() {
-	
+const add = function(x,y) {
+	let sum = x + y;
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	const sum = x - y;
+  return sum
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let finalSum = 0;
+  for (x of args) {
+    if (typeof x === 'object') {
+      for (let i = 0; i < x.length; i++) {
+        finalSum += x[i]
+      }
+      return finalSum
+    } 
+    finalSum += x;
+  }
+  return finalSum
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let product = 1;
+  for (x of args) {
+    if (typeof x === 'object') {
+      for (let i = 0; i < x.length; i++) {
+        product *= x[i]
+      }
+      return product
+    } 
+    product *= x;
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let product = 1;
+  for (let i = 1; i <= y; i++)	{
+    product *= x; 
+  }
+  return product
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let product = 1;
+  while (x > 0) {
+    product *= x;
+    x--;
+  }
+  return product
 };
 
 // Do not edit below this line
