302ed575544c09b20215209d0da7611c944e70a4
joe <joe.gdowik@gmail.com>
2022-03-05 20:21:08 -0500

    palindromes was tricky

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) { 
+  let bool = true;
+  //normalizing the input to all lowercase, no punctuation, etc.
+  string = string.replace(/[.,'"\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  string = string.replace(/\s/g,"");
+  string = string.toLowerCase();
+  let array = string.split(''); //string becomes an array for ease of use
 
-};
+  //function to reverse the string
+  function reverse(array) {
+    let revArray = [];
+      for (let i = 0; i < array.length; i++) {
+          revArray.unshift(array[i]);
+      }
+      return revArray
+  };
 
+  let reversed = reverse(array);
+  //so now we have two arrays of lowercase letters that we have to compare
+  
+  if (array.length !== reversed.length) {
+    bool = false
+  }
+  
+  for (let i=0; i<array.length; i++) {
+    if (array[i] !== reversed[i]) {
+      bool = false
+    }
+  }
+  
+  return bool
+}
 // Do not edit below this line
 module.exports = palindromes;
