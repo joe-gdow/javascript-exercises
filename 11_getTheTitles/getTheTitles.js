9d82d1dd411aaa5cba5713bc2670e82a7fdfa7c7
joe <joe.gdowik@gmail.com>
2022-03-06 23:46:21 -0500

    finished my js exercises according to the odin project foundations

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //take array as an argument and create a new array of book titles taken from objects in the array
+    const titles = books.map(book => book.title);
+    return titles
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  /*  given an array of people objects, return the oldest person object (death - birth) 
+      if one of the person objects does not have a death year (is still alive), find current age using JS date function */
 
-};
 
-// Do not edit below this line
+      //to get current date: Date().split(' ')
+      //returns an array in the order Day Month Date Year HH:mm:ss Timezone
+      const currentYear = Date().split(' ')[3];
+      const oldest = [];
+      let age = 0;
+
+      //first a function to help us find everyone's age
+      function findAge(person) {
+        if (!person.yearOfDeath) { //if they're not dead
+          return currentYear - person.yearOfBirth;//use the current year we found earlier to determine age
+        }
+        else { return person.yearOfDeath - person.yearOfBirth }
+      }
+
+      //Then, loop thru the array comparing the result of the findAge function to a 
+      //placeholder variable that stores the oldest age
+      people.map((person) => {
+        if (findAge(person)>age) {
+          age = findAge(person);
+          oldest.splice(0, 1, person); //add that object to our return array
+        }
+      });
+      return oldest[0] //return the first object in the return array
+    }
+
+//Don't edit below this line
 module.exports = findTheOldest;
